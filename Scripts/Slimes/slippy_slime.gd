443d7c8b0481f514c033b5c1b0b59a3ef0832e0a
greatbrick <speharley@gmail.com>
2024-08-18 02:41:03 -0400

    Added Slippy Slime And Adjusted Camera Angles

---
+++ Scripts/Slimes/slippy_slime.gd
@@ -0,0 +1,23 @@
+extends Node
+
+func AttemptMove(dir: Vector2):
+	while(MoveOneTile(dir) and get_parent().DropHeightLayer() == 0):
+		pass
+
+func MoveOneTile(dir: Vector2):
+	if(IsOnlyAir(get_parent().CheckFacingTiles(dir))):
+		$"../MovePlayer".play("Move" + VectorTools.VectorToString(dir))
+		$"../Visuals".position = VectorTools.Vec2ToVec3(-dir)
+		get_parent().MoveTiles(dir)
+		return true
+	else:
+		$"../Visuals".position = Vector3.ZERO
+		$"../MovePlayer".play("Bump" + VectorTools.VectorToString(dir))
+		return false
+
+func IsOnlyAir(tiles: Array[String]):
+	var output: bool = true
+	for ii in tiles:
+		if(ii != "Air"):
+			output = false
+	return output
