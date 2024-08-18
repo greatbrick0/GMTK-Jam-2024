4abb76fa7558448fd25b743828273ede0171e80a
greatbrick <speharley@gmail.com>
2024-08-18 17:23:46 -0400

    Added Button Theme

--- Scripts/PlayTestButton.gd
+++ Scripts/PlayTestButton.gd
@@ -3,7 +3,7 @@ extends Node
 @export var newScene: PackedScene
 
 func _ready():
-	self.button_down.connect(LoadScene)
+	self.pressed.connect(LoadScene)
 
 func LoadScene():
 	get_tree().change_scene_to_packed(newScene)
