040ed4bcf021418c67ffa617ee0de966163bd644
greatbrick <speharley@gmail.com>
2024-08-18 20:26:37 -0400

    Changed Move Logic

--- Scripts/Slimes/jumpy_slime.gd
+++ Scripts/Slimes/jumpy_slime.gd
@@ -1,19 +1,27 @@
 extends Node
 
 func AttemptMove(dir: Vector2):
-	MoveOneTile(dir)
-	MoveOneTile(dir)
-	get_parent().DropHeightLayer()
-	SlimeController.CheckForVictory()
-
-func MoveOneTile(dir: Vector2):
-	if(IsOnlyAir(get_parent().CheckFacingTiles(dir))):
+	if(MoveOneTile(dir)):
 		$"../MovePlayer".play("Move" + VectorTools.VectorToString(dir))
 		$"../Visuals".position = VectorTools.Vec2ToVec3(-dir)
-		get_parent().MoveTiles(dir)
 	else:
 		$"../Visuals".position = Vector3.ZERO
 		$"../MovePlayer".play("Bump" + VectorTools.VectorToString(dir))
+	if(MoveOneTile(dir)):
+		$"../MovePlayer".play("Move" + VectorTools.VectorToString(dir))
+		$"../Visuals".position = VectorTools.Vec2ToVec3(-dir)
+	else:
+		$"../Visuals".position = Vector3.ZERO
+		$"../MovePlayer".play("Bump" + VectorTools.VectorToString(dir))
+	get_parent().DropHeightLayer()
+	SlimeController.CheckForVictory()
+
+func MoveOneTile(dir: Vector2) -> bool:
+	if(IsOnlyAir(get_parent().CheckFacingTiles(dir))):
+		get_parent().MoveTiles(dir)
+		return true
+	else:
+		return false
 
 func IsOnlyAir(tiles: Array[String]):
 	var output: bool = true
--- Scripts/Slimes/slippy_slime.gd
+++ Scripts/Slimes/slippy_slime.gd
@@ -1,19 +1,23 @@
 extends Node
 
 func AttemptMove(dir: Vector2):
-	while(MoveOneTile(dir) and get_parent().DropHeightLayer() == 0):
-		pass
-	SlimeController.CheckForVictory()
-
-func MoveOneTile(dir: Vector2):
-	if(IsOnlyAir(get_parent().CheckFacingTiles(dir))):
+	var didMove: bool = MoveOneTile(dir)
+	if(didMove):
 		$"../MovePlayer".play("Move" + VectorTools.VectorToString(dir))
 		$"../Visuals".position = VectorTools.Vec2ToVec3(-dir)
-		get_parent().MoveTiles(dir)
-		return true
 	else:
 		$"../Visuals".position = Vector3.ZERO
 		$"../MovePlayer".play("Bump" + VectorTools.VectorToString(dir))
+	
+	while(IsOnlyAir(get_parent().CheckFacingTiles(dir)) and get_parent().DropHeightLayer() == 0):
+		get_parent().MoveTiles(dir)
+	SlimeController.CheckForVictory()
+
+func MoveOneTile(dir: Vector2) -> bool:
+	if(IsOnlyAir(get_parent().CheckFacingTiles(dir))):
+		get_parent().MoveTiles(dir)
+		return true
+	else:
 		return false
 
 func IsOnlyAir(tiles: Array[String]):
