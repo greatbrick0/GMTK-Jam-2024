fda40cd693462a29cc752a08e508e59f6173d394
greatbrick <speharley@gmail.com>
2024-08-19 01:35:38 -0400

    Added Combination Dictionary

--- Scripts/slime.gd
+++ Scripts/slime.gd
@@ -1,7 +1,17 @@
 extends Node3D
 class_name Slime
 
+enum slimeTypes {
+	Large = 0,
+	Basic = 1,
+	Slippy = 2,
+	Angry = 3,
+	Jumpy = 4,
+	Brainy = 5
+}
+
 @export var init: bool = false
+@export var slimeName: slimeTypes = slimeTypes.Basic
 @export var heightLayer: int = 1
 @export var tilePosition: Vector2i = Vector2i.ZERO
 var isDead: bool = false
@@ -9,6 +19,24 @@ var isDead: bool = false
 @export var inControl: bool = false
 var inGoal: bool = false
 var isBig: bool
+
+const slimeComboMap: Dictionary = {
+	[1, 1]: "res://Scenes/Slimes/Big/large_basic_slime.tscn",
+	[1, 2]: "res://Scenes/Slimes/Big/large_basic_slime.tscn",
+	[1, 3]: "res://Scenes/Slimes/Big/large_basic_slime.tscn",
+	[1, 4]: "res://Scenes/Slimes/Big/large_basic_slime.tscn",
+	[1, 5]: "res://Scenes/Slimes/Big/large_basic_slime.tscn",
+	[2, 2]: "res://Scenes/Slimes/Big/large_basic_slime.tscn",
+	[2, 3]: "res://Scenes/Slimes/Big/large_basic_slime.tscn",
+	[2, 4]: "res://Scenes/Slimes/Big/large_basic_slime.tscn",
+	[2, 5]: "res://Scenes/Slimes/Big/large_basic_slime.tscn",
+	[3, 3]: "res://Scenes/Slimes/Big/large_basic_slime.tscn",
+	[3, 4]: "res://Scenes/Slimes/Big/large_basic_slime.tscn",
+	[3, 5]: "res://Scenes/Slimes/Big/large_basic_slime.tscn",
+	[4, 4]: "res://Scenes/Slimes/Big/large_basic_slime.tscn",
+	[4, 5]: "res://Scenes/Slimes/Big/large_basic_slime.tscn",
+	[5, 5]: "res://Scenes/Slimes/Big/large_basic_slime.tscn",
+	}
 
 func ChangeTiles(newPos: Vector2i):
 	tilePosition = newPos
