8491dfb0f73eb7ab945762aac8f31fd2d70bf0a8
Alexander <xrdharley@gmail.com>
2024-08-20 02:02:22 -0400

    Merge branch 'main' of https://github.com/greatbrick0/GMTK-Jam-2024

--- Scripts/Slimes/skippy_slime.gd
+++ Scripts/Slimes/skippy_slime.gd
@@ -10,19 +10,30 @@ func AttemptMove(dir: Vector2):
 		else:
 			$"../AnimQueue".AddMoveToQueue(dir, "Bump", 0.0)
 	
+	var fallDist: int
 	var ii: int = 1
-	while(didMove and get_parent().DropHeightLayer() == 0):
+	while(didMove):
 		didMove = MoveOneTile(dir)
 		if(didMove):
 			$"../AnimQueue".AddMoveToQueue(dir, "Move", ii * 0.5)
 		else:
 			$"../AnimQueue".AddMoveToQueue(dir, "Bump", ii * 0.5)
+		
+		if(!get_parent().CheckForGround() and !CheckForWalls(dir)):
+			fallDist = get_parent().DropHeightLayer()
+			if(fallDist != 0):
+				didMove = false
+				$"../AnimQueue".AddFallToQueue(fallDist, (ii + 1) * 0.5)
 		ii += 1
 	
-	var fallDist: int = get_parent().DropHeightLayer()
-	if(fallDist != 0):
-		didMove = false
-		$"../AnimQueue".AddFallToQueue(fallDist, (ii + 1) * 0.5)
+	fallDist = get_parent().DropHeightLayer()
+	if(fallDist != 0): $"../AnimQueue".AddFallToQueue(fallDist, (ii + 1) * 0.5)
 	
 	SlimeController.CheckForVictory()
 	$"../AnimQueue".ExecuteQueue()
+
+func CheckForWalls(dir: Vector2i) -> bool:
+	for ii in range(1, 20):
+		if(!IsOnlyAir(get_parent().CheckFacingTiles(dir * ii))):
+			return true
+	return false
--- Scripts/Slimes/slippy_slime.gd
+++ Scripts/Slimes/slippy_slime.gd
@@ -11,13 +11,18 @@ func AttemptMove(dir: Vector2):
 			$"../AnimQueue".AddMoveToQueue(dir, "Bump", 0.0)
 	
 	var ii: int = 1
-	while(didMove and get_parent().DropHeightLayer() == 0):
+	var fallDist: int = get_parent().DropHeightLayer()
+	if(fallDist != 0):
+			didMove = false
+			$"../AnimQueue".AddFallToQueue(fallDist, (ii + 1) * 0.5)
+	
+	while(didMove):
 		didMove = MoveOneTile(dir)
 		if(didMove):
 			$"../AnimQueue".AddMoveToQueue(dir, "Move", ii * 0.5)
 		else:
 			$"../AnimQueue".AddMoveToQueue(dir, "Bump", ii * 0.5)
-		var fallDist: int = get_parent().DropHeightLayer()
+		fallDist = get_parent().DropHeightLayer()
 		if(fallDist != 0):
 			didMove = false
 			$"../AnimQueue".AddFallToQueue(fallDist, (ii + 1) * 0.5)
