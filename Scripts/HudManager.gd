db0d7070da9bfb77faef70ea298e6212611e5fd7
greatbrick <speharley@gmail.com>
2024-08-20 04:06:49 -0400

    Added Control Buttons

--- Scripts/HudManager.gd
+++ Scripts/HudManager.gd
@@ -1,5 +1,15 @@
 extends CanvasLayer
 
+@export var winSounds: Array[AudioStream]
+
+func _process(delta):
+	if(SlimeController.slimeList.size() > 1):
+		$Control/ControlButtons.visible = true
+		for ii in range($Control/ControlButtons.get_children().size()):
+			$Control/ControlButtons.get_children()[ii].visible = SlimeController.slimeList.size() > ii
+	else:
+		$Control/ControlButtons.visible = false
+
 func FadeOut():
 	$FadePlayer.play("FadeOut")
 	await $FadePlayer.animation_finished
@@ -7,3 +17,7 @@ func FadeOut():
 func FadeIn():
 	$FadePlayer.play("FadeIn")
 	await $FadePlayer.animation_finished
+
+func PlayWinSound():
+	$WinSound.stream = winSounds.pick_random()
+	$WinSound.play()
